f2e7cafc71d66d75057ea22bdba5fab709871875
Fabio Tani <fabio.tani@nextt.com.br>
2022-09-22 08:51:02 +0200

    Update code (#4)
    * Renaming Modes and Tiers to Tiers and Licenses to better match gitlab model
    
    * Separate filtering for tier and license

--- src/filter/GitlabReleaseFilter.ts
+++ src/filter/GitlabReleaseFilter.ts
@@ -22,17 +22,26 @@ export const filterReleasePage = ({tier = Tiers.all, license = Licenses.all}: Fi
   let affected = 0;
 
   if (tier !== Tiers.all || license !== Licenses.all) {
-    const unavailable_xpath = `//div[contains(@class, 'badge-container-type') and text()='${tier}']/following-sibling::div/a/div[text()='${license}' and not(contains(@class,"available"))]/ancestor::div[contains(@class, "release-row") and not(contains(@class, "divider")) or contains(@class, "secondary-column-feature")]`;
+    let tierFilter = ``;
+    if (tier !== Tiers.all) {
+      tierFilter = ` and text()='${tier}'`;
+    }
+    let licenseFilter = ``;
+    if (licenseFilter !== Licenses.all) {
+      licenseFilter = `text()='${license}' and `;
+    }
+  
+    const unavailable_xpath = `//div[contains(@class, 'badge-container-type')${tierFilter}]/following-sibling::div/a/div[${licenseFilter}not(contains(@class,"available"))]/ancestor::div[contains(@class, "release-row") and not(contains(@class, "divider")) or contains(@class, "secondary-column-feature")]`;
 
-    const un = document.evaluate(unavailable_xpath, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    const unavailable = document.evaluate(unavailable_xpath, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
     
     try {
-      let thisNode = un.iterateNext();
+      let thisNode = unavailable.iterateNext();
       affected++;
     
       while (thisNode) {
         (thisNode as HTMLElement).style.display = "none";
-        thisNode = un.iterateNext();
+        thisNode = unavailable.iterateNext();
         affected++;
       }
     }
